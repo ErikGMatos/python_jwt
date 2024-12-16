14dc7a1cbb7d62b548921ecef5865b1bc4a862f7
Erik Garces Matos <erik_senac@hotmail.com>
2024-12-16 11:10:47 -0300

    feat: Implementing LoginCreator with tests

---
+++ src/controllers/login_creator_test.py
@@ -0,0 +1,31 @@
+
+
+import pytest
+
+from src.controllers.login_creator import LoginCreator
+from src.drivers.password_handler import PasswwordHandler
+
+USERNAME = "meuUsername"
+PASSWORD = "minhaSenha"
+HASHED_PASSWORD = PasswwordHandler().encrypt_password(PASSWORD)
+
+
+class MockUserRepository:
+    def get_user_by_username(self, mock_username):
+        return (10, mock_username, HASHED_PASSWORD)
+
+
+def test_create():
+    login_creator = LoginCreator(MockUserRepository())
+    response = login_creator.create(USERNAME, PASSWORD)
+
+    assert response["access"] is True
+    assert response["username"] == USERNAME
+    assert response["token"] is not None
+
+
+def test_create_with_wrong_password():
+    login_creator = LoginCreator(MockUserRepository())
+
+    with pytest.raises(Exception):
+        login_creator.create(USERNAME, "outraSenha")
